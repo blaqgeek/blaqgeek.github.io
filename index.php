3a373da58f61f9dea82362f5ab1361bdab698797
Godfred Yeleboue <blaqgeek@users.github.com>
2020-04-25 02:53:00 +0000

    update

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if(isset($_POST['submit'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> myWebsite</title>
+    <title> Blaq Geek | Helping others is my passion.</title>
 
     <!-- Font Awesome  -->
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
