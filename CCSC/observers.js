32ca112275fcd003e8d494333983e7fde1a49fa7
Godfred Yeleboue <blaqgeek@users.github.com>
2020-04-27 21:05:03 +0000

    updates

---
+++ CCSC/observers.js
@@ -0,0 +1,38 @@
+
+// Header on scroll
+const header = document.querySelector(".header");
+const sectionOne = document.querySelector(".carousel-inner");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+// Menu on mobile screen
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+
+//Smooth scrolling
+const scroll = new SmoothScroll('a[href*="#"]',{
+  speed: 1200
+});
